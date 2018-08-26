9202927d139de41195e0963498b1a83b3361fec9
Anuoluwa Apiti <mercies101@gmail.com>
2018-08-26 01:51:30 +0100

    [Feature #159952829] Create endpoints to get all questions and a question and add a middleware to verify token

--- api/v2/controllers/questionController.js
+++ api/v2/controllers/questionController.js
@@ -1,6 +1,16 @@
 import pool from '../config/config';
 
+/** Class representing a question. */
 export default class Questions {
+  /**
+ * /GET all questions
+ *
+ * @async
+ * @function get all questions in the database
+ * @param {req} url - The request obj that handles request that is coming in.
+ * @param {res} url - The response obj that handles response from request.
+ * @return {HTTP status<objec>, json} The rows of data  from the URL.
+ */
   static async GetAllQuestions(req, res) {
     const { rows } = await pool
       .query('SELECT * FROM questions');
@@ -14,6 +24,15 @@ export default class Questions {
     }
   }
 
+  /**
+ * /GET a single question
+ *
+ * @async
+ * @function get a question in the database
+ * @param {req} url - The request obj that handles request that is coming in.
+ * @param {res} url - The response obj that handles response from request.
+ * @return {HTTP status<objec>, json} The rows of data  from the URL.
+ */
   static async GetOneQuestion(req, res) {
     const { rows } = await pool
       .query('SELECT * FROM questions WHERE question_id = $1',
