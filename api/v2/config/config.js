5f5147d46681542de72a990c9e8ecbda03e9c17a
Anuoluwa Apiti <mercies101@gmail.com>
2018-08-31 02:40:47 +0100

    [Chore ] Fix the broken ap on Heroku and change config to ElephanSQL

--- api/v2/config/config.js
+++ api/v2/config/config.js
@@ -4,21 +4,10 @@ import { Pool } from 'pg';
 dotenv.config();
 
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.ELEPHANTSQL_URL;
 const pool = new Pool({
   connectionString,
 
 });
-
-// let connectionString;
-
-// if (process.NODE_ENV) {
-//   if (process.NODE_ENV.trim() === 'test') connectionString = process.env.ELEPHANT_TEST;
-
-//   if (process.NODE_ENV.trim() === 'production') connectionString = process.env.ELEPHANTSQL_URL;
-// }
-// const pool = new Pool({
-//   connectionString,
-// });
 
 export default pool;
